da593f17f9ba92b1a3a596a94dbd586cb6f9a27f
Shai Rubinstein <rubinsh@gmail.com>
2014-03-18 22:46:35 +0200

    fixed orbit slider

--- app/assets/javascripts/controllers/modelGalleryController.js
+++ app/assets/javascripts/controllers/modelGalleryController.js
@@ -1,13 +1,17 @@
 angular.module('autoControllers')
-    .controller('ModelGalleryCtrl', ['$scope', '$routeParams', 'GalleryServices',
-        function($scope, $routeParams, GalleryServices) {
+    .controller('ModelGalleryCtrl', ['$scope', '$routeParams','$timeout', 'GalleryServices',
+        function($scope, $routeParams,$timeout, GalleryServices) {
             $scope.gallery_id = $routeParams.id;
 
             if ($scope.gallery_id) {
                 GalleryServices.getAllModelGalleryByGalleryId($scope.gallery_id)
                     .success(function(data) {
                         $scope.gallery = data;
-                        console.log(data);
+                        //use $timeout with 0 to run code after the dom finished rendering by angular
+                        //see http://stackoverflow.com/questions/12240639/angularjs-how-can-i-run-a-directive-after-the-dom-has-finished-rendering
+                        $timeout(function(){ 
+                          $(document).foundation();
+                        });
                     });
             }
         }
